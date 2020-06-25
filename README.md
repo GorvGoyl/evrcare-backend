ba125ea5083e72ed1e4ed0b08515a1e321d5b7c6
jerrygoyal <1gouravgg@gmail.com>
2020-06-25 21:57:59 +0530

    pwsh n scripts setup

--- README.md
+++ README.md
@@ -1,10 +1,79 @@
+# Guidelines
+
+- follow guidelines from evrcare-app repo
+- use cross platform Powershell for commands to avoid any platform specific syntax issues https://github.com/powershell/powershell#get-powershell. Tips: Use `&` for parallel commands, `;` for sequential commands, To stop running commands: ctrl+C
+
 # Multiple ENV
 
 - check: `firebase use`
 - use specific env for run & debug: `firebase use prod`
 - deploy to specific env: `firebase deploy -P dev --only functions`
 
+# RUN
+
+- use `package.json` scripts to run, debug, & deploy
+
+`firebase serve` // to run hosting + functions
+`firebase serve --host 0.0.0.0` // to also run in mobile
+`firebase serve --host 0.0.0.0 --only functions`
+
+- to point local firestore, add below to firebase json "emulators"
+  "firestore": {
+  "port": 8080
+  },
+
+# DEBUG FUNCTIONS
+
+- use `package.json` scripts to run, debug, & deploy
+
+## vscode debugger
+
+1. `npm run hotReload`
+2. `firebase emulators:start --only functions --inspect-functions`
+3. Run `Attach` or press `F5`
+
+## see functions logs in terminal
+
+https://firebase.google.com/docs/functions/local-shell
+
+2. `firebase functions:shell` //execute trigger functions in terminal and look for logs
+3. or `firebase serve` //look for terminal logs
+
+## see logs of deployed functions in vscode console
+
+- `firebase functions:log --only <method name>` //for specific function
+
+- `firebase functions:log` //for all functions
+
+- `firebase help functions:log` //see help
+
+# DEPLOY
+
+- use `package.json` scripts to run, debug, & deploy
+
 # SERVER LOCATION
 
 - `GCP` & `firestore` region: `asia-south1` (mumbai)
 - `functions` region: `asia-east 2` (HK)
+
+## Emulators
+
+to test emulators paste after functions object in firebase.json
+
+```
+,
+  "emulators": {
+    "functions": {
+      "port": 5001
+    },
+    "firestore": {
+      "port": 8080
+    },
+    "pubsub": {
+      "port": 8085
+    },
+    "ui": {
+      "enabled": true
+    }
+  }
+```
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,52 @@
-import * as functions from "firebase-functions";
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
+// eslint-disable-next-line import/first
+// import * as serviceAccount from "../dev-adminsdk.json";
+// import sgMail = require("@sendgrid/mail");
+// import util = require("util");
+// import axios from "axios";
+
+admin.initializeApp();
+// admin.initializeApp(functions.config().firebase);
+// admin.initializeApp({
+//   // below serviceAccount is only used for local serve and debug
+//   credential: admin.credential.cert(serviceAccount as any),
+
+// });
+const LOCATION = "asia-east2";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
+export const bookService = functions
+  .region(LOCATION)
+  .https.onCall(async (data, context) => {
+    // validating a request
+    if (!data) {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "data doesnt exist"
+      );
+    }
+    validateUser(context);
+
+    return admin
+      .firestore()
+      .collection("orders")
+      .add(data)
+      .then(() => {
+        console.log("added a order successfully");
+        return { response: "yeah" };
+      })
+      .catch((err) => {
+        console.log(err);
+
+        throw new functions.https.HttpsError(
+          "internal",
+          "some internal error try back later"
+        );
+      });
+  });
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
@@ -11,3 +55,35 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 export const helloWorld2 = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+// #region ## ----------------- utility methods ----------------- ##
+// validate a user if it authenticated && has evrcare email and it is verified
+function validateUser(context: functions.https.CallableContext): void {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("unauthenticated", "no user found");
+  }
+
+  console.log(
+    context.auth.uid,
+    context.auth.token.email,
+    context.auth.token.email_verified
+  );
+
+  if (!context.auth.token.email) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "user has no email id please update your profile"
+    );
+  }
+
+  // if(!context.auth.token.email.match(/[a-z0-9._-]+@gmail+\.com/)){
+  //     throw new functions.https.HttpsError("permission-denied","your are trying to access restriced resources!");
+  // }
+
+  if (!context.auth.token.email_verified) {
+    throw new functions.https.HttpsError(
+      "permission-denied",
+      "email not verified"
+    );
+  }
+}
